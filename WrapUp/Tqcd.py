1e76d039a3d21b43c03882ea9daf4ab3d90d7d26
Yi-Ting <kaliduh@gmail.com>
2018-05-02 12:57:03 +0200

    Tqcd.py: adding more kinematics all in the output rootfiles, no SB separate script needed

--- WrapUp/Tqcd.py
+++ WrapUp/Tqcd.py
@@ -12,10 +12,8 @@ nominal = sys.argv[2]
 skimType = 'skimrootSB' if nominal == "noEW" else 'skimrootSB_'+nominal
 outfile = r.TFile("./%s/%s/skim_Tqcd.root" %(njets,skimType), "RECREATE")
 
-kint = Varargs().DefaultVarDict['_Mtt_delY'].saveName+"_RECO"
-
 #=====================================================================
-def getQCDshape(skimType, strnominal):
+def getQCDshape(skimType, strnominal, info):
 	fdata = r.TFile("./%s/%s/skim_DATA.root" %(njets,skimType))
 	ftt = r.TFile("./%s/%s/skim_tt_PowhegP8_%s.root" %(njets,skimType,strnominal))
 	ft = r.TFile("./%s/%s/skim_t.root" %(njets,skimType))
@@ -23,6 +21,7 @@ def getQCDshape(skimType, strnominal):
 	fww = r.TFile("./%s/%s/skim_WW.root" %(njets,skimType))
 	fwz = r.TFile("./%s/%s/skim_WZ.root" %(njets,skimType))
 
+	kint = info.saveName+'_RECO'
 	hdata = fdata.Get(kint)
 	htt = ftt.Get(kint)
 	ht = ft.Get(kint)
@@ -32,90 +31,98 @@ def getQCDshape(skimType, strnominal):
 
 	hsum = htt.Clone()
 	hsum.Add(ht, 1)
-	#hsum.Add(hvj, 1)
-	#hsum.Add(hww, 1)
-	#hsum.Add(hwz, 1)
+	hsum.Add(hvj, 1)
+	hsum.Add(hww, 1)
+	hsum.Add(hwz, 1)
 
 	Tqcd = hdata.Clone()
 	Tqcd.Add(hsum, -1)
 	numdata = Tqcd.Integral()
 	Tqcdtemp = Tqcd.Clone()
-	errdata = Tqcdtemp.Rebin2D(Tqcdtemp.GetNbinsX(),Tqcdtemp.GetNbinsY())
-	errdata = errdata.GetBinError(1, 1)
+	errdata = Tqcdtemp.Rebin2D(Tqcdtemp.GetNbinsX(),Tqcdtemp.GetNbinsY()) if info.getHistDim() == 2 else Tqcdtemp.Rebin(Tqcdtemp.GetNbinsX())
+	errdata = errdata.GetBinError(1, 1) if info.getHistDim() == 2 else errdata.GetBinError(1)
 	Tqcd.Scale(1/Tqcd.Integral())
 
-	hTqcd = r.TH2D(Varargs().DefaultVarDict['_Mtt_delY'].saveName+"_RECO", Varargs().DefaultVarDict['_Mtt_delY'].saveName+"_RECO", Tqcd.GetNbinsX(), 0, 2000, Tqcd.GetNbinsY(), -6, 6)
-	hTqcdX = r.TH1D(Varargs().DefaultVarDict['_Mtt'].saveName+"_RECO", Varargs().DefaultVarDict['_Mtt'].saveName+"_RECO", Tqcd.GetNbinsX(), 0, 2000)
-	hTqcdY = r.TH1D(Varargs().DefaultVarDict['_delY'].saveName+"_RECO", Varargs().DefaultVarDict['_delY'].saveName+"_RECO", Tqcd.GetNbinsY(), -6, 6)
+	hTqcd = r.TH2D(kint, kint, Tqcd.GetNbinsX(), Tqcd.GetXaxis().GetBinLowEdge(1), Tqcd.GetXaxis().GetBinUpEdge(Tqcd.GetNbinsX()), Tqcd.GetNbinsY(), Tqcd.GetYaxis().GetBinLowEdge(1), Tqcd.GetYaxis().GetBinUpEdge(Tqcd.GetNbinsY())) if info.getHistDim() == 2 else r.TH1D(kint, kint, Tqcd.GetNbinsX(), Tqcd.GetXaxis().GetBinLowEdge(1), Tqcd.GetXaxis().GetBinUpEdge(Tqcd.GetNbinsX()))
 
-	for i in range(Tqcd.GetNbinsX()):
-		for j in range(Tqcd.GetNbinsY()):
-			if(Tqcd.GetBinContent(i+1, j+1) >= 0):
-				hTqcd.SetBinContent(i+1, j+1, Tqcd.GetBinContent(i+1, j+1))
+	if info.getHistDim() == 2:
+		for i in range(Tqcd.GetNbinsX()):
+			for j in range(Tqcd.GetNbinsY()):
+				if(Tqcd.GetBinContent(i+1, j+1) >= 0):
+					hTqcd.SetBinContent(i+1, j+1, Tqcd.GetBinContent(i+1, j+1))
+					hTqcd.SetBinError(i+1, j+1, Tqcd.GetBinError(i+1, j+1))
+				else:
+					hTqcd.SetBinContent(i+1, j+1, 0)
+					hTqcd.SetBinError(i+1, j+1, Tqcd.GetBinError(i+1, j+1))
+	else:
+		for i in range(Tqcd.GetNbinsX()):
+			if(Tqcd.GetBinContent(i+1) >=0):
+				hTqcd.SetBinContent(i+1, Tqcd.GetBinContent(i+1))
+				hTqcd.SetBinError(i+1, Tqcd.GetBinError(i+1))
 			else:
-				hTqcd.SetBinContent(i+1, j+1, 0)
+				hTqcd.SetBinContent(i+1, 0)
+				hTqcd.SetBinError(i+1, Tqcd.GetBinError(i+1))
 
-	hTqcdX = hTqcd.ProjectionX()
-	hTqcdY = hTqcd.ProjectionY()
-
-	return numdata, errdata, hTqcd, hTqcdX, hTqcdY,[fdata,ftt,ft,fvj,fww,fwz]
+	return numdata, errdata, hTqcd, [fdata,ftt,ft,fvj,fww,fwz]
 
 #=====================================================================
-def getQCDnorm(skimType, numdataSB, errdata):
+def getQCDnorm(skimType, numdataSB, errdata, info):
 	fqcdSR = r.TFile("./%s/skimroot/skim_QCD.root" %njets)
 	fqcdSB = r.TFile("./%s/%s/skim_QCD.root" %(njets,skimType))
 
+	#kint = info.saveName+'_RECO'
+	kint = Varargs().DefaultVarDict['_Mtt_delY'].saveName+"_RECO"
 	numqcdSR = fqcdSR.Get(kint).Integral()
 	numqcdSB = fqcdSB.Get(kint).Integral()
 	numqcd = (numqcdSR/numqcdSB)*numdataSB
 	norm = numqcd
-	errqcdSR = fqcdSR.Get(kint).Rebin2D(fqcdSR.Get(kint).GetNbinsX(), fqcdSR.Get(kint).GetNbinsY())
-	errqcdSR = errqcdSR.GetBinError(1, 1)
-	errqcdSB = fqcdSB.Get(kint).Rebin2D(fqcdSB.Get(kint).GetNbinsX(), fqcdSB.Get(kint).GetNbinsY())
-	errqcdSB = errqcdSB.GetBinError(1, 1)
-	print "This is the case of csv region", skimType, ":"
-	print "Norm =", norm, "+-", sqrt((errqcdSR/numqcdSR)**2+(errqcdSB/numqcdSB)**2+(errdata/numdataSB)**2)*norm
-	print "MC Signal Region yields =", numqcdSR, "+-", errqcdSR
-	print "MC Sideband      yields =", numqcdSB, "+-", errqcdSB
-	print "data Sideband    yields =", numdataSB, "+-", errdata
+
+	if info.getHistDim() == 2:
+		errqcdSR = fqcdSR.Get(kint).Rebin2D(fqcdSR.Get(kint).GetNbinsX(), fqcdSR.Get(kint).GetNbinsY())
+		errqcdSR = errqcdSR.GetBinError(1, 1)
+		errqcdSB = fqcdSB.Get(kint).Rebin2D(fqcdSB.Get(kint).GetNbinsX(), fqcdSB.Get(kint).GetNbinsY())
+		errqcdSB = errqcdSB.GetBinError(1, 1)
+		print "This is the case of csv region", skimType, ":"
+		print "Norm =", norm, "+-", sqrt((errqcdSR/numqcdSR)**2+(errqcdSB/numqcdSB)**2+(errdata/numdataSB)**2)*norm
+		print "MC Signal Region yields =", numqcdSR, "+-", errqcdSR
+		print "MC Sideband      yields =", numqcdSB, "+-", errqcdSB
+		print "data Sideband    yields =", numdataSB, "+-", errdata
+		print "====================================================="
+		print "norm uncert in Combine", sqrt((errqcdSR/numqcdSR)**2+(errqcdSB/numqcdSB)**2+(errdata/numdataSB)**2)
 
 	return norm
 
 #=====================================================================
 
+#kint = Varargs().DefaultVarDict['_Mtt_delY'].saveName+"_RECO"
+varargs = Varargs().DefaultVarDict
+varargs.update(Varargs().AddVarDict)
 
-numdataCR, errdataCR, hTqcdCR, hTqcdCRX, hTqcdCRY,_ = getQCDshape('skimrootSB', 'noEW') #The shape of csv[0-0.6]
-normCR = getQCDnorm('skimrootSB', numdataCR, errdataCR) #The normalization of csv[0-0.6]
+for histname, info in varargs.iteritems():
+	print info.saveName
+	numdataCR, errdataCR, hTqcdCR, _ = getQCDshape('skimrootSB','noEW',info) #The shape of csv[0-0.6]
+	normCR = getQCDnorm('skimrootSB', numdataCR, errdataCR, info) #The shape/normalization of csv[0-0.6]
 
-if nominal == 'noEW':
-	hTqcdCR.Scale(normCR)
-	hTqcdCRX.Scale(normCR)
-	hTqcdCRY.Scale(normCR)
+	if nominal == 'noEW':
+		hTqcdCR.Scale(normCR)
+		print "Using norm =", normCR
 
-	outfile.cd()
-	hTqcdCR.Write()
-	hTqcdCR.SetName('qcd')
-else:
-	numdata, errdata, hTqcd, hTqcdX, hTqcdY,flist = getQCDshape(skimType, nominal) #The shape of any feed-in csv region
-	hTqcd.Scale(normCR) #Shpae is defined by any feed-in csv region, but the normalization is applied with which csv[0-0.6]
-	hTqcdX.Scale(normCR)
-	hTqcdY.Scale(normCR)
+		outfile.cd()
+		hTqcdCR.Write()
+		hTqcdCR.SetName('qcd')
+	else:
+		numdata, errdata, hTqcd, flist = getQCDshape(skimType,nominal,info) #The shape of any feed-in csv region
+		hTqcd.Scale(normCR) #Shpae is defined by any feed-in csv region, but the normalization is applied with which csv[0-0.6]
+		print "Using norm =", normCR
 
-	outfile.cd()
-	hTqcd.Write()
-	hTqcd.SetName('qcd')
+		outfile.cd()
+		hTqcd.Write()
+		hTqcd.SetName('qcd')
 
-	#The print out helps you to compare btw up/down templates norm uncertainties
-	norm = getQCDnorm(skimType, numdata, errdata) #The normalization of any feed-in csv regsion
-	#hTqcd.Scale(norm) #Shpae is defined by any feed-in csv region, but the normalization is applied with which csv[0-0.6]
-	#hTqcdX.Scale(norm)
-	#hTqcdY.Scale(norm)
-	#outfile.cd()
-	#hTqcd.Write()
-	#hTqcd.SetName('qcd')
+		#The print out helps you to compare btw up/down templates norm uncertainties
+		norm = getQCDnorm(skimType, numdata, errdata, kint) #The normalization of any feed-in csv regsion
+		#hTqcd.Scale(norm) #Shpae is defined by any feed-in csv region, but the normalization is applied with which csv[0-0.6]
 
-#for f in flist:
-#	print f
 outfile.Close()
 
 
